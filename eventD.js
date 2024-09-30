43075a1039ac9225d09667828a8d4bf60fc32876
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-09-30 12:49:42 +0500

    Event delegation handling

--- eventD.js
+++ eventD.js
@@ -1,1 +1,17 @@
 // How Event Delegation works
+
+// Selecting parent element
+
+const evenDelegation = document.querySelector('.eventDelegation')
+
+const title = document.querySelector('.title')
+// evenDelegation.style.background = 'red'
+
+evenDelegation.addEventListener('click', (event)=> {
+    if(event.target.tagName === "BUTTON"){
+        console.log("Button Clicked")
+        title.textContent = "This is event delegation"
+        title.style.color = "red"
+    }
+})
+// console.log(evenDelegation)
